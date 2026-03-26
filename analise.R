f3a32832863943230eabff1c0dfbb16ad840f05b
andreeler1 <andreeler@gmail.com>
2026-03-25 22:22:57 -0300

    Update analise.R
    exercício 5

--- analise.R
+++ analise.R
@@ -7,9 +7,8 @@ municipios_virgula <- read.csv("C:/Users/aeler/Desktop/Métodos Quantitativos/da
 
 municipios_pontovirgula <- read.csv2("C:/Users/aeler/Desktop/Métodos Quantitativos/dados/municipios_pontovirgula.csv", fileEncoding = "latin1")
 
-
-#exercício 3#
 library(dplyr)  
+tinytex::install_tinytex()
 
 eleicoes_2022 <- read.csv("C:/Users/aeler/Desktop/Métodos Quantitativos/dados/eleicoes_2022.CSV", fileEncoding = "Latin1")
 eleicoes_2022 %>% 
@@ -19,7 +18,5 @@ eleicoes_2022 %>%
 idh_municipios <- read.csv2("C:/Users/aeler/Desktop/Métodos Quantitativos/dados/idh_municipios.CSV", fileEncoding = "Latin1")
 str(idh_municipios)
 
-install.packages("tinytex")
-tinytex::install_tinytex()
 
 
